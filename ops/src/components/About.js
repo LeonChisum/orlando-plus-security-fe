2ecf4ded452073fce168a07967cf47b6697fe370
Leon Chisum <leonchisum3306@gmail.com>
2020-02-21 14:50:18 -0500

    About us fixed styling, updated headshots

--- ops/src/components/About.js
+++ ops/src/components/About.js
@@ -1,26 +1,27 @@
-import React, { useState } from 'react'
-import { Card, Icon, Image, Grid } from 'semantic-ui-react'
+import React, { useState } from "react";
+import { Card, Icon, Image, Grid } from "semantic-ui-react";
 
 // imported data
-import { managementData } from '../data/data'
-import ManagementCard from './ManagementCard'
+import { managementData } from "../data/data";
+import ManagementCard from "./ManagementCard";
 
 export default function About() {
+  const [management, setManagment] = useState(managementData);
 
-    const [management, setManagment] = useState(managementData)
-
-    return (
-        <div className='team-background'>
-            <section className='sub-header'>
-                <h4>MEET THE TEAM</h4>
-                <p>Together Everyone Achieves More</p>
-            </section>
-            <section className='team-container'>
-                {management.map(i => {
-                    return <ManagementCard key={i.id} manager={i} />
-                })}
-            </section>
-            {/* <Card.Group itemsPerRow='three'>
+  return (
+    <div className="team-background">
+      <section className="sub-header">
+        <h4>MEET THE TEAM</h4>
+        <p>Together Everyone Achieves More</p>
+      </section>
+      <section className="team-container">
+        <div className="card-wrapper">
+          {management.map(i => {
+            return <ManagementCard key={i.id} manager={i} />;
+          })}
+        </div>
+      </section>
+      {/* <Card.Group itemsPerRow='three'>
                     <Card>
                         <Image src={MC} wrapped ui={false} />
                         <Card.Content>
@@ -79,6 +80,6 @@ export default function About() {
                     </Card>
             
             </Card.Group> */}
-        </div>
-    )
+    </div>
+  );
 }
--- ops/src/data/data.js
+++ ops/src/data/data.js
@@ -1,28 +1,31 @@
 // imported images
-import MC from '../images/MC Headshot.jpg'
-import CH from '../images/CH Headshot.JPG'
-import TW from '../images/TW Headshot.JPG'
+import MC from "../images/MC Headshot.jpg";
+import CH from "../images/CH Headshot.JPG";
+import TW from "../images/TW Headshot.JPG";
 
 export const managementData = [
-    {
-        name: 'Mike Callaghan',
-        image: MC,
-        title: 'President',
-        bio: 'Mike is the President of the company',
-        id: 1,
-    },
-    {
-        name: 'Christine Hayward',
-        image: CH,
-        title: 'Vice President',
-        bio: 'Christine is the Vice President of the Company',
-        id: 2,
-    },
-    {
-        name: 'Tristen Wong',
-        image: TW,
-        title: 'Director of Operations',
-        bio: 'Tristen is the Director of Operations of the Company',
-        id: 3,
-    }
-]
+  {
+    name: "Mike Callaghan",
+    image: MC,
+    title: "President",
+    bio:
+      "Michael retired from the Chicago Police Department after 22 years of service. He has been working in the trade show industry since 1987. Michael has experience working on-site, managing the many large events as well as security consulting in Orlando, Las Vegas, New Orleans, Chicago and San Diego, to name a few. Michael is pleased to announce his new title as President of Orlando Plus Security & Event Staffing (OPS). OPS will be providing security & event staffing to trade shows and conventions in the Central Florida area. Michael’s expertise includes but is not limited to, Executive protection, Crisis Management, Counter-terrorism, Emergency Management, Security Operations, Homeland Security, Asset Protection and Access Control.",
+    id: 1
+  },
+  {
+    name: "Christine Hayward",
+    image: CH,
+    title: "Vice President",
+    bio:
+      "Christine graduated from University of Central Florida with a double major in Marketing and Finance. She has been in the hospitality and event industry since childhood. Christine has many years of experience in preparing and facilitating security plans with both show management and the venue. She can assist in coordinating and contracting Off Duty Law Enforcement, K9s, and Event Medical services for events and incorporating them into one contract. Christine has knowledge with events in Orlando, Las Vegas and New Orleans.",
+    id: 2
+  },
+  {
+    name: "Tristen Wong",
+    image: TW,
+    title: "Director of Operations",
+    bio:
+      "Tristan joined the Century Security & Event Staffing in Orlando, Florida in February of 2012 as an Operations Supervisor, and was promoted to Operations Manager in 2014.  With a strong technical background supported by production and management experience, Tristan manages daily on-site security operations in New Orleans, Las Vegas, Tucson, and Orlando.  He has also participated as management on events in New York and Washington D.C.  In addition, Tristan’s responsibilities include equipment maintenance, lost and found management, and oversees all CCTV equipment and installation.",
+    id: 3
+  }
+];
