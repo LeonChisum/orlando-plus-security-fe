24d0424c7a0352c58b3f1ce8ff83e8efe9735d80
LeonChisum <leonchisum3306@gmail.com>
2020-02-25 10:05:05 -0500

    updated Tristan's bio

--- ops/src/components/Contact.js
+++ ops/src/components/Contact.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ContactForm from "./ContactForm"
+import UnderConstruction from './UnderConstruction'
 
 export default function Contact() {
   return (
     <div>
-      <section className="sub-header">
+      {/* <section className="sub-header">
         <h4>NEED MORE INFORMATION?</h4>
         <p>Send us a message!</p>
       </section>
       <section className="team-container">
         <ContactForm />
-      </section>
+      </section> */}
+      <UnderConstruction />
     </div>
   );
 }
