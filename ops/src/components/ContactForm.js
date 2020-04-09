e26b8cfc6d068f99749f274a875c4a073abcb8a8
Leon Chisum <leonchisum3306@gmail.com>
2020-04-09 13:26:20 -0400

    still working on form

--- ops/src/components/ContactForm.js
+++ ops/src/components/ContactForm.js
@@ -3,44 +3,24 @@ import { Form, Segment, Button } from "semantic-ui-react";
 
 export default function ContactForm() {
 
-  const clientObj = {
-    name: "",
-    email: "",
-    message: ""
-  }
-  const [client, setClient] = useState(clientObj);
-
-  const handleChange = e => {
-    setClient({ ...client, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    // setClient(clientObj);
-  };
   return (
     <div className='form-container'>
-      <form className='form' name='Contact Info' method='POST' netlify>
+      <form className='form' name='Contact Info' method='post'>
+      <input type="hidden" name="form-name" value="contact" />
         <input
           type="text"
           name="name"
           placeholder="Name"
-          value={client.name}
-          onChange={handleChange}
         ></input>
         <input
           type="email"
           name="email"
           placeholder="Email"
-          value={client.email}
-          onChange={handleChange}
         ></input>
         <textarea
           type="text"
           name="message"
           placeholder="Enter a Message..."
-          value={client.message}
-          onChange={handleChange}
         ></textarea>
         <button>SEND</button>
       </form>
