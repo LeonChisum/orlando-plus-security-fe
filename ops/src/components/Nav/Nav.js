28342a9dcee1bf2737ee084066431ae0a5181767
LeonChisum <leonchisum3306@gmail.com>
2020-11-24 13:05:13 -0500

    updated hamburger nav bar for mobile

--- ops/src/components/Nav/Nav.js
+++ ops/src/components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 // images and css
@@ -6,30 +6,36 @@ import logo from '../../images/OPSLogo.png'
 import "../Nav/Nav.css"
 
 export default function Nav() {
+    const [menuHidden, setMenuHidden] = useState('closed')
+
     return (
         <div className='header'>
             <img src={logo} alt='OPS logo' className='logo' />
             <div className='nav-list'>
-                <div className='nav-btn'>
-                    <div className='nav-btn-burger' />
+                {/* menuHidden toggles the hamburger nav menu when clicked */}
+               <div className={`nav-btn ${menuHidden}`} onClick={() => setMenuHidden('open')}>
+                        <div className='nav-btn-burger' />
+                </div> 
+                <div className={`nav-menu ${menuHidden}`} onClick={() => setMenuHidden('closed')}>
+                    <div className='x'>X</div>
+                    <ul>
+                        <li>
+                            <Link to='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/services'>Services</Link>
+                        </li>
+                        <li>
+                            <Link to='/about'>About Us</Link>
+                        </li>
+                        <li>
+                            <Link to='/contact'>Contact</Link>
+                        </li>
+                        <li>
+                            <Link to='/securitydtraining'>Training</Link>
+                        </li>
+                    </ul> 
                 </div>
-                <ul>
-                    <li>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link to='/services'>Services</Link>
-                    </li>
-                    <li>
-                        <Link to='/about'>About Us</Link>
-                    </li>
-                    <li>
-                        <Link to='/contact'>Contact</Link>
-                    </li>
-                    <li>
-                        <Link to='/securitydtraining'>Training</Link>
-                    </li>
-                </ul>
             </div>
         </div>
     )
