6109cf96061929bc69e4ce7521bd779384f65726
LeonChisum <leonchisum3306@gmail.com>
2020-12-03 20:37:32 -0500

    fixed bug in data component

--- ops/src/data/data.js
+++ ops/src/data/data.js
@@ -1,6 +1,6 @@
 // imported images
 import MC from "../images/MC Headshot.jpg";
-import CH from "../images/CH Headshot.jpg";
+import CH from "../../src/images/CH Headshot.JPG";
 import TW from "../images/TW Headshot2.jpg";
 
 export const managementData = [
